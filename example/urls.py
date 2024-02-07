5eabaab9c8e13364bd271bc86205f0355c6f46ca
Paul Starodub <amorallex@gmail.com>
2024-02-07 16:00:24 +0200

    update urls.py

--- example/urls.py
+++ example/urls.py
@@ -3,6 +3,7 @@ from example import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("show_messages/", views.show_messages, name="show_messages"),
 ]
 
 app_name = "example"

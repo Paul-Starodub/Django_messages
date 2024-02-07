b4d9f42e0d223fc7f461ef582e2ffde189b1934a
Paul Starodub <amorallex@gmail.com>
2024-02-07 15:40:15 +0200

    update views.py

--- example/views.py
+++ example/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        print(request.POST["email"])
+        print(request.POST["password"])
+
+        if request.POST["password"] == "password":
+            messages.error(request, "Your password isn't complex enough")
+            return redirect("example:index")
+
+    return render(request, "example/index.html")
+
+
+def show_messages(request: HttpRequest) -> HttpResponse:
+    return render(request, "example/show_messages.html")
